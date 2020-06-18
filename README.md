e73a055b53f43ac40f83785301dae82782545847
anko-chan <61561412+anko-chan@users.noreply.github.com>
2020-06-19 05:24:54 +0900

    ver0.8位相当？

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # FF14Party-bot
 Discord用FF14もパーティー募集ごっこをするbot
+現在1bot1サーバー用
 
+
+#通常コマンド
+!PT "募集タイトル" "募集文"
+で8人PTを募集可能。間は半角スペースのみ
+
+!PT4 "募集タイトル" "募集文"
+で4人PTを募集。
+
+#管理者機能
+settings.iniにDiscordのSetting＞Appearance＞Advanced＞Developer Modeをオンにして、サーバー上のメンバーリストから自分を右クリック、Copy IDで得られるIDを入力すると一部コマンドが使えるようになります。
+
+!PTcheck
+現在アクティブな募集とそれの参加者をリスト出力。
+
+!shutdown
+botをシャットダウン
 
 # todo
+募集分にかぶり禁止とpt人数記載
+強制出発コマンド
+募集削除コマンド
+!help
+これ
+ちゃんとreactionを消す処理
 await create_custom_emoji(*, name, image, roles=None, reason=None)
--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import os
 import errno
 import logging
 
+
+#logger-------------------------------------------
 client = discord.Client()
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
+#--------------------------------------------------
 
 #____________________________________
 #config読み込み
@@ -27,14 +30,23 @@ config.read(config_ini_path, encoding='utf-8')
 #権限設定取得
 boss = config['Admin']['AdminID']
 
+
+#PT情報保存用リスト
+#PTinfo = [messageid, author, title, desc, is4ppl, allowdupe] * maxparty
+PTinfo = [[0] * 4] * int(config['Settings']['maxparty'])
+PTmember = [[0] * 8] * int(config['Settings']['maxparty'])
+PTrole = [[0] * 3] * int(config['Settings']['maxparty'])
 #______________________________________
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    print(client.user)
 
     #todo
     #guildを取得したい
+    #client.guildsでリスト取得可能
+
 
 @client.event
 async def on_message(message):
@@ -53,6 +65,26 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
 
+    """emojiをconfigparserに入れたいと思ったけどコメントが消えてしまうので断念
+    #emoji init---------------------------------------------------------------
+    if message.content.startswith('!init'):
+        if message.author.id == int(boss):
+            pld_em = discord.utils.get(message.guild.emojis, name='PLD')
+            war_em = discord.utils.get(message.guild.emojis, name='WAR')
+            drk_em = discord.utils.get(message.guild.emojis, name='DRK')
+            gnb_em = discord.utils.get(message.guild.emojis, name='GNB')
+            config["Emojis"] = {"Pld": pld_em,
+                                "War": war_em,
+                                "Drk": drk_em,
+                                "Gnb": gnb_em}
+
+            with open("settings.ini", "w") as configfile:
+                config.write(configfile)
+            print("initiated!")
+        else:
+            await message.channel.send('ask boss for init')
+            return
+    """
 
     #diceroll------------------------------------------------------------
     if message.content.startswith('!roll'):
@@ -113,20 +145,35 @@ async def on_message(message):
     #Partyfinder-------------------------------------------------------------
     #PT番号
     PTnum = 0
-    #PT情報保存用リスト
-    #[messageid, authorid, title, desc, ]
-    PTinfo = [[0] * 4] * int(config['Settings']['maxparty'])
-
 
     if message.content.startswith("!PT") or message.content.startswith("!pt"):
+
+        #PT登録内容チェック----------------------------------------------------------
+        if message.content.startswith('!PTcheck'):
+            if message.author.id == int(boss):
+                for i in range(len(PTinfo)):
+                    await message.channel.send(str(PTinfo[i]) + "\n"
+                                                + str(PTmember[i][0]) + ", "
+                                                + str(PTmember[i][1]) + ", "
+                                                + str(PTmember[i][2]) + ", "
+                                                + str(PTmember[i][3]) + ", "
+                                                + str(PTmember[i][4]) + ", "
+                                                + str(PTmember[i][5]) + ", "
+                                                + str(PTmember[i][6]) + ", "
+                                                + str(PTmember[i][7]))
+                return
+
+            else:
+                await message.channel.send("Who's there?")
+                return
+        #--------------------------------------------------------------------------
 
         # PT募集チャンネル未設定or設定チャンネル上の場合
         if (int(config['Settings']['PTfindchannel']) == 0) or (message.channel.id == int(config['Settings']['PTfindchannel'])):
             #await message.channel.send("LookingForPT")
 
-            #todo 同時募集PT数制限
 
-            PTnum += 1
+
             PTdesc = message.content.split(" ")
 
             #出力
@@ -135,69 +182,219 @@ async def on_message(message):
             # <b><u>Title</b></u>
             # desc~~~
 
-            #Messageidを取得。保存。emoji付け
+            #Messageidを取得。保存。PTmemberとPTrole初期化
             PTfindmsg_id = PTfindmsg.id
             PTfindmsg_guild = PTfindmsg.guild
 
+            #PTinfo = [messageid, author, title, desc, is4ppl, allowdupe] * maxparty
+            if "4" in PTdesc[0]:
+                PTinfo[PTnum] = [PTfindmsg_id, message.author, PTdesc[1], PTdesc[2], True, False]
+            else:
+                PTinfo[PTnum] = [PTfindmsg_id, message.author, PTdesc[1], PTdesc[2], False, False]
+
+            #かぶり禁止
+            if ("k" or "K") in PTdesc[0]:
+                PTinfo[PTnum][5] = True
+
+            PTmember[PTnum] = [0] * 8
+            PTrole[PTnum] = [0] * 3
+
+            print("PTnum:" + str(PTnum) + " msgid:" + str(PTfindmsg_id) + " Author:" + str(PTfindmsg.author) + " Title:" + str(PTdesc[1]) + " desc:" + str(PTdesc[2]))
+            print(PTinfo)
+
+
+
+            PTnum += 1
+            if PTnum == int(config['Settings']['maxparty']):
+                PTnum = 0
 
             #各種emoji
-            pld_em = discord.utils.get(PTfindmsg.guild.emojis, name='PLD')
-            war_em = discord.utils.get(PTfindmsg.guild.emojis, name='WAR')
-            drk_em = discord.utils.get(PTfindmsg.guild.emojis, name='DRK')
-            gnb_em = discord.utils.get(PTfindmsg.guild.emojis, name='GNB')
+            jobemotelist = getjobemotes(message)
+            print(jobemotelist)
 
-            drg_em = discord.utils.get(PTfindmsg.guild.emojis, name='DRG')
-            mnk_em = discord.utils.get(PTfindmsg.guild.emojis, name='MNK')
-            nin_em = discord.utils.get(PTfindmsg.guild.emojis, name='NIN')
-            sam_em = discord.utils.get(PTfindmsg.guild.emojis, name='SAM')
-
-            brd_em = discord.utils.get(PTfindmsg.guild.emojis, name='BRD')
-            mch_em = discord.utils.get(PTfindmsg.guild.emojis, name='MCH')
-            dnc_em = discord.utils.get(PTfindmsg.guild.emojis, name='DNC')
-
-            smn_em = discord.utils.get(PTfindmsg.guild.emojis, name='SMN')
-            blm_em = discord.utils.get(PTfindmsg.guild.emojis, name='BLM')
-            rdm_em = discord.utils.get(PTfindmsg.guild.emojis, name='RDM')
-
-            whm_em = discord.utils.get(PTfindmsg.guild.emojis, name='WHM')
-            sch_em = discord.utils.get(PTfindmsg.guild.emojis, name='SCH')
-            ast_em = discord.utils.get(PTfindmsg.guild.emojis, name='AST')
 
             async for message in PTfindmsg.channel.history(limit=10):
                 if message.id == PTfindmsg_id:
-                    await message.add_reaction(pld_em)
-                    await message.add_reaction(war_em)
-                    await message.add_reaction(drk_em)
-                    await message.add_reaction(gnb_em)
+                    for job in jobemotelist:
+                        await message.add_reaction(job)
 
-                    await message.add_reaction(drg_em)
-                    await message.add_reaction(mnk_em)
-                    await message.add_reaction(nin_em)
-                    await message.add_reaction(sam_em)
-
-                    await message.add_reaction(brd_em)
-                    await message.add_reaction(mch_em)
-                    await message.add_reaction(dnc_em)
-
-                    await message.add_reaction(smn_em)
-                    await message.add_reaction(blm_em)
-                    await message.add_reaction(rdm_em)
-
-                    await message.add_reaction(whm_em)
-                    await message.add_reaction(sch_em)
-                    await message.add_reaction(ast_em)
-            #emojiを付けたユーザーを保存、リアクション削除、かぶり検知
-            #揃ったらメンションでｼｬｷｰﾝ
 
         # 設定されたPT募集チャンネルに送信
         else:
             await message.channel.send("PT募集用チャンネルを使ってください")
 
-    #Partyfinder, emoji付け
 
-    #emoji 検索
-    #job_emoji = discord.utils.get(guild.emojis, name='BLM')
-    #if job_emoji:
-    #    await message.add_reaction(job_emoji)
+
+
+@client.event
+async def on_reaction_add(reaction, user):
+    #botのreaction付け感知
+    if user == client.user:
+        #print("no")
+        return
+
+    #どうでもいいリアクションが追加されたときに消す
+    if reaction.count == 1:
+        await reaction.remove(user)
+        return
+
+    #ロールリアクションが追加されたとき
+    if reaction.count > 1 and reaction.me == True:
+        #PTinfo内のメッセージ判別
+
+        inv_msg = reaction.message
+        reaction_emj = reaction.emoji
+
+        for i in range(int(config['Settings']['maxparty'])):
+            #print(str(i) + "：" + str(inv_msg.id) + "：" + str(PTinfo[i][0]))
+            if inv_msg.id == PTinfo[i][0]:
+
+                """
+                #reactionした人がすでにPTにいる場合
+                if user in PTmember[i]:
+                    warn = await inv_msg.channel.send(user.mention + "そのPTには参加済みだよ！")
+                    await reaction.remove(user)
+                    await warn.delete(delay=10)
+                    return
+                """
+
+                #reactionした人のidをPTmemberに追加
+                PTmember[i][PTrole[i][0] + PTrole[i][1] + PTrole[i][2]] = user
+                print("user stored to party list")
+
+                jobemotelist = getjobemotes(inv_msg)
+
+                #タンク数管理
+                if (reaction_emj == jobemotelist[0]
+                    or reaction_emj == jobemotelist[1]
+                    or reaction_emj == jobemotelist[2]
+                    or reaction_emj == jobemotelist[3]):
+
+                    PTrole[i][0] += 1
+
+                elif (reaction_emj == jobemotelist[4]
+                    or reaction_emj == jobemotelist[5]
+                    or reaction_emj == jobemotelist[6]
+                    or reaction_emj == jobemotelist[7]
+                    or reaction_emj == jobemotelist[8]
+                    or reaction_emj == jobemotelist[9]
+                    or reaction_emj == jobemotelist[10]
+                    or reaction_emj == jobemotelist[11]
+                    or reaction_emj == jobemotelist[12]
+                    or reaction_emj == jobemotelist[13]):
+
+                    PTrole[i][1] += 1
+
+                elif (reaction_emj == jobemotelist[14]
+                    or reaction_emj == jobemotelist[15]
+                    or reaction_emj == jobemotelist[16]):
+
+                    PTrole[i][2] += 1
+
+                print("tankcount=" + str(PTrole[i][0]) +
+                      "\ndpscount=" + str(PTrole[i][1]) +
+                      "\nhealercount=" + str(PTrole[i][2]))
+
+
+
+                #タンクが規定数以上参加していた場合残りのタンクを削除
+                if (PTinfo[i][4] == True and PTrole[i][0] > 0) or PTrole[i][0] > 1:
+                        for rc in inv_msg.reactions:
+                            if (str(rc) == str(jobemotelist[0])
+                                or str(rc) == str(jobemotelist[1])
+                                or str(rc) == str(jobemotelist[2])
+                                or str(rc) == str(jobemotelist[3])):
+                                await rc.clear()
+
+
+                #dpsが規定数参加していたらemote削除
+                #全部ひとまとめだと消しきれないことがあったので分散させてみる
+                #だめでした
+                if (PTinfo[i][4] == True and PTrole[i][1] > 1) or PTrole[i][1] > 3:
+                    for rc in inv_msg.reactions:
+                        if (str(rc) == str(jobemotelist[4])
+                            or str(rc) == str(jobemotelist[6])
+                            or str(rc) == str(jobemotelist[8])
+                            or str(rc) == str(jobemotelist[10])
+                            or str(rc) == str(jobemotelist[12])
+                            or str(rc) == str(jobemotelist[5])
+                            or str(rc) == str(jobemotelist[7])
+                            or str(rc) == str(jobemotelist[9])
+                            or str(rc) == str(jobemotelist[11])
+                            or str(rc) == str(jobemotelist[13])):
+                            await rc.clear()
+
+                #ヒーラーが規定数以上参加していた場合削除
+                if (PTinfo[i][4] == True and PTrole[i][2] > 0) or PTrole[i][2] > 1:
+                    for rc in inv_msg.reactions:
+                        if (str(rc) == str(jobemotelist[14])
+                            or str(rc) == str(jobemotelist[15])
+                            or str(rc) == str(jobemotelist[16])):
+                            await rc.clear()
+
+                print("emote added, tank, dps, and healer adjusted")
+
+                #かぶり禁止の場合押されたemoteを削除
+                if PTinfo[i][5] == True:
+                    await reaction.clear()
+
+                #8人揃った処理
+                if PTrole[i][0] + PTrole[i][1] + PTrole[i][2] > 7:
+                    await inv_msg.channel.send("Full Party! \n" +
+                                                        "**__" + str(PTinfo[i][2]) + "__**\n" +
+                                                        PTmember[i][0].mention + " " +
+                                                        PTmember[i][1].mention + " " +
+                                                        PTmember[i][2].mention + " " +
+                                                        PTmember[i][3].mention + " " +
+                                                        PTmember[i][4].mention + " " +
+                                                        PTmember[i][5].mention + " " +
+                                                        PTmember[i][6].mention + " " +
+                                                        user.mention)
+
+                    #募集メッセージ削除
+                    await inv_msg.delete(delay=3)
+
+                elif PTinfo[i][4] == True and PTrole[i][0] + PTrole[i][1] + PTrole[i][2] > 3:
+                    await inv_msg.channel.send("Light Party! \n" +
+                                                        "**__" + str(PTinfo[i][2]) + "__**\n" +
+                                                        PTmember[i][0].mention + " " +
+                                                        PTmember[i][1].mention + " " +
+                                                        PTmember[i][2].mention + " " +
+                                                        user.mention)
+                    await inv_msg.delete(delay=3)
+
+
+#msgを渡して、鯖からエモートを取得しリスト化
+def getjobemotes(msg):
+    pld_em = discord.utils.get(msg.guild.emojis, name='PLD')
+    war_em = discord.utils.get(msg.guild.emojis, name='WAR')
+    drk_em = discord.utils.get(msg.guild.emojis, name='DRK')
+    gnb_em = discord.utils.get(msg.guild.emojis, name='GNB')
+
+    drg_em = discord.utils.get(msg.guild.emojis, name='DRG')
+    mnk_em = discord.utils.get(msg.guild.emojis, name='MNK')
+    nin_em = discord.utils.get(msg.guild.emojis, name='NIN')
+    sam_em = discord.utils.get(msg.guild.emojis, name='SAM')
+
+    brd_em = discord.utils.get(msg.guild.emojis, name='BRD')
+    mch_em = discord.utils.get(msg.guild.emojis, name='MCH')
+    dnc_em = discord.utils.get(msg.guild.emojis, name='DNC')
+
+    smn_em = discord.utils.get(msg.guild.emojis, name='SMN')
+    blm_em = discord.utils.get(msg.guild.emojis, name='BLM')
+    rdm_em = discord.utils.get(msg.guild.emojis, name='RDM')
+
+    whm_em = discord.utils.get(msg.guild.emojis, name='WHM')
+    sch_em = discord.utils.get(msg.guild.emojis, name='SCH')
+    ast_em = discord.utils.get(msg.guild.emojis, name='AST')
+
+
+    return [pld_em, war_em, drk_em, gnb_em,
+            drg_em, mnk_em, nin_em, sam_em,
+            brd_em, mch_em, dnc_em,
+            smn_em, blm_em, rdm_em,
+            whm_em, sch_em, ast_em]
+
+
 
 client.run(str(config['bot']['BotToken']))
